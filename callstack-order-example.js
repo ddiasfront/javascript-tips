eee73bc5adf69052b609b1c0e0777743164706da
Diego Dias <diegodias@Diegos-MacBook-Pro.local>
2022-06-19 17:41:29 -0300

    Micro and macro tasks

--- callstack-order-example.js
+++ callstack-order-example.js
@@ -21,14 +21,14 @@
  }
 
  const personPresentation = (person) => {
-   console.log('Callback 1');
+   console.log('1 Executed on callstack');
    return `Hello, my name is ${person.name}, im ${person.age} and a ${person.profession} ! `;
  }
 
  const contryPresentation = (countries) => {
-   console.log('Callback 2');
-   return `My dream is work as a software engineer in ${countries.uk.city} and to be living in ${countries.uk.district} !`;
- }
+   console.log('2  Executed on callstack');
+   return `I'd really like to live in ${countries.uk.city} - ${countries.uk.district} and enjoy your team!`;
+ } 
 
  const integratePersonAndContry = (personPresentationCallback, contryPresentationCallback) => {
    const res1 = personPresentationCallback(person);
@@ -40,16 +40,33 @@ const presentationResult = integratePersonAndContry(personPresentation, contryPr
 
 console.log(presentationResult);
 
-console.log('Main code completed');
+console.log('3 Main code completed');
 
 
 //MICRO TASK
+   ///PROMISES
+   const afterPresenting = (isAsync) => {
+      return new Promise(resolve =>{ 
+         setTimeout(() => {
+            resolve(console.log(`${isAsync ? '6 Executed after the macro cause of timeout even in micro' : '4 Executed before macro cause out of timeout and in micro'}`));
+         }, isAsync ? 5000 : 0);
+      });
+   }
+
+   const asyncPresenting = async(isAsync) => {
+      await afterPresenting(isAsync);
+      console.log('7 And this will execute only after getting the timeout done even in micro');
+      
+   }
+   afterPresenting()
+   asyncPresenting(true);
+
+   
+
 // MACRO TASK
-
-
-// setTimeout(() => {
-//    console.log('Task 1 Completed') 
-// }, 200);
+setTimeout(() => {
+   console.log('5 This, will execute before finishing the async operation wich was already triggered, although is a macro task') 
+});
 
 
 
--- readme.md
+++ readme.md
@@ -8,10 +8,12 @@ EVENT LOOP
 ```sh
 1st Scope => Call CallStack 
 ( synchhrounous functions LIFO Last in First out )
+-- Inversion of control
 
 2nd Scope => Microtasks ( Here is where the browser stops )
 
-(process.nextTick, Promises, Object.observe, MutationObserver)
+(process.nextTick, Promises ES6 , Object.observe, MutationObserver, async await ES8)
+
 3rd Scope => Macro Tasks ( Here is the last scope to be executed )
 ( tTimeout, setInterval, setImmediate, requestAnimationFrame, I/O, UI rendering )
 ```
